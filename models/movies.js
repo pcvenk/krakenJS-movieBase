8a9b7da5b4f9f6afdf0d0610b54b4c43727bbc7b
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-02-14 18:15:17 +0100

    Setting up the DB schema

--- models/movies.js
+++ models/movies.js
@@ -1,7 +1,19 @@
 'use strict';
 
-module.exports = function MoviesModel() {
-    return {
-        name: 'movies'
-    };
+var moviesModel = function(){
+    //defining a new schema
+    var movieSchema = mongoose.Schema({
+        title: String,
+        genre: String,
+        plot: String,
+        director: String,
+        releaseDate: Date,
+        trailer: String,
+        cover: String
+    });
+
+    return mongoose.model('Movie', movieSchema);
+
 };
+
+module.exports = moviesModel();
