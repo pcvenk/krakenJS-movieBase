6b41b33d4ac265b81805cdca1aec6427bda145f8
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-02-17 18:49:17 +0100

    Delete functionality on the front-end

--- .build/js/app.js
+++ .build/js/app.js
@@ -1,20 +1,21 @@
-/*global requirejs:true*/
-'use strict';
-
-
-requirejs.config({
-    paths: {}
-});
-
-
-require([/* Dependencies */], function () {
-
-    var app = {
-        initialize: function () {
-            // Your code here
-        }
-    };
-
-    app.initialize();
-
+$(document).ready(function() {
+    $('.delete-movie').click(function(event) {
+        $target = $(event.target);
+        $.ajax({
+            type: 'DELETE',
+            url: '/movies/delete/' + $target.attr('data-movie-id'),
+            data: {
+                _csrf: $target.attr('data-csrf')
+            },
+            success: function(response) {
+                $target.parent().parent().remove();
+                alert('Deleting Movie');
+                window.location.href = '/movies';
+            },
+            error: function(error) {
+                alert(error);
+                console.log(error);
+            }
+        })
+    });
 });
