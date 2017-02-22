f9fb09790c2600e1208e77dc96ddf1e0469abd67
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-02-22 16:01:36 +0100

    Restructuring the index route

--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,23 @@
 'use strict';
 
-var IndexModel = require('../models/index');
-
+var Movie = require('../models/movies');
 
 module.exports = function (router) {
 
-    var model = new IndexModel();
+    var model = new Movie();
 
     router.get('/', function (req, res) {
-        
-        
-        res.render('index', model);
-        
-        
+        //get all movies
+        Movie.find({}, function(err, docs){
+            if(err){
+                res.send(err);
+            } else {
+                res.render('movies', {
+                    movies: docs
+                });
+            }
+        });
+
     });
 
 };
